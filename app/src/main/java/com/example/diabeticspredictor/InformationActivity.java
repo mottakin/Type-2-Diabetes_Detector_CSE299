c7ffa80bd6849b6610660998215143f0621008ac
mottakin <mottakinsayem@gmail.com>
2020-08-02 22:28:49 +0600

    Three screens and two buttons

--- app/src/main/java/com/example/diabeticspredictor/InformationActivity.java
+++ app/src/main/java/com/example/diabeticspredictor/InformationActivity.java
@@ -2,13 +2,25 @@ package com.example.diabeticspredictor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class InformationActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
+        button = (Button) findViewById(R.id.Button3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InformationActivity.this, FoodActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
