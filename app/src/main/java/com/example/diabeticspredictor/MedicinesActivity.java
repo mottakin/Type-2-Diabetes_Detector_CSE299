0aac666ac99717b6b5b7f1277758738f846fac2a
mottakin <mottakinsayem@gmail.com>
2020-09-17 20:54:49 +0600

    Medicine Part

--- app/src/main/java/com/example/diabeticspredictor/MedicinesActivity.java
+++ app/src/main/java/com/example/diabeticspredictor/MedicinesActivity.java
@@ -2,13 +2,71 @@ package com.example.diabeticspredictor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
-public class MedicinesActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class MedicinesActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private int notificationId = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medicines);
+
+        findViewById(R.id.setBtn).setOnClickListener(this);
+        findViewById(R.id.cancelBtn).setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view)
+    {
+        EditText editTextMedicine = findViewById(R.id.editTextMedicine);
+        TimePicker timePicker = findViewById(R.id.timePicker);
+
+       Intent intent = new Intent(MedicinesActivity.this, AlarmReceiver.class);
+       intent.putExtra("notificationId", notificationId);
+       intent.putExtra("todo", editTextMedicine.getText().toString());
+
+        PendingIntent alarmIntent = PendingIntent.getBroadcast(MedicinesActivity.this,0,intent,PendingIntent.FLAG_CANCEL_CURRENT);
+        AlarmManager alarm = (AlarmManager) getSystemService(ALARM_SERVICE);
+        switch (view.getId())
+        {
+            case R.id.setBtn:
+                int hour = timePicker.getCurrentHour();
+                int minute = timePicker.getCurrentMinute();
+
+                Calendar startTime = Calendar.getInstance();
+                startTime.set(Calendar.HOUR_OF_DAY, hour);
+                startTime.set(Calendar.MINUTE, minute);
+                startTime.set(Calendar.SECOND,0);
+                long alarmStartTime = startTime.getTimeInMillis();
+
+                alarm.set(AlarmManager.RTC_WAKEUP, alarmStartTime, alarmIntent);
+
+                Toast.makeText(this, "Done!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.cancelBtn:
+                alarm.cancel(alarmIntent);
+                Toast.makeText(this, "Canceled.", Toast.LENGTH_SHORT).show();
+                break;
+
+
+
+
+
+        }
+
+    }
+
+
 }
